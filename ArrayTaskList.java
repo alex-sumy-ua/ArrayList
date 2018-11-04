1be9b1fb915ff1eb2e706cd1ba3ff2672a3db411
alex-sumy-ua <plachkovskyy@gmail.com>
2018-11-05 01:08:04 +0200

    Add files via upload

---
+++ ArrayTaskList.java
@@ -0,0 +1,114 @@
+//package ua.sumdu.j2se.plachkovskyy.tasks;
+
+/**
+ * Class ArrayTaskList for the list of tasks
+ */
+public class ArrayTaskList {
+
+    private int     listLength  = 10;                   // array start length
+    private Task[]  taskList    = new Task[listLength]; // list of tasks
+    int             i;                                  // array index
+
+    /**
+     *   Constructor of empty list
+     */
+    public ArrayTaskList() {
+        for (i = 0; i < listLength; i++) {
+            taskList[i] = null;
+        }
+    }
+
+    /**
+     * Adding new element (task) into list
+     */
+    public void add(Task task) {
+        for (i = 0; i < taskList.length; i++) {
+            if (taskList[i] == null) {
+                taskList[i] =   task;
+                break;
+            }
+        }
+        // Checking/correction array size
+        if ((size() / taskList.length) > 0.75) {
+            listLength   =   (int) (taskList.length * 1.3);
+            Task[]  tempList    = new Task[listLength];
+            System.arraycopy(taskList, 0, tempList, 0, taskList.length);
+            taskList = tempList;
+        }
+    }
+
+    /**
+     * Removes only first appropriate element from list.
+     * Returns true if element was found and removed,
+     * returns false if element was not found.
+     */
+    public boolean remove(Task task) {
+        boolean done = false;
+        for (i = 0; i < listLength; i++) {
+            if (taskList[i].equals(task)) {
+                taskList[i] = null;
+                done = true;
+                // Checking/correction array size
+                if ((size() / listLength) < 0.5) {
+                    listLength   =   (int) (listLength * 0.75);
+                    Task[]  tempList    = new Task[listLength];
+                    System.arraycopy(taskList, 0, tempList, 0, i);
+                    System.arraycopy(taskList, ++i, tempList, i-1, listLength - i);
+                    taskList = tempList;
+                }
+                break;
+            }
+        }
+        return done;
+    }
+
+    /**
+     * Returns tasks total
+     */
+    public int size() {
+        int realSize = 0;
+        for (i = 0; i < listLength; i++) {
+            if (taskList[i] != null) {
+                realSize += 1;
+            }
+        }
+        return realSize;
+    }
+
+    /**
+     * Returns Task by index
+     */
+    public Task getTask(int index) {
+        if (index < listLength) {
+            return taskList[index];
+        } else {
+            return null;
+        }
+    }
+
+    /**
+     * Returns tasks list, which will be planned at least once
+     * after "from" and not later than "to"
+     */
+    public Task[] incoming(int from, int to) {
+        Task[] incomingList    =   new Task[listLength];
+        for (i = 0; i < listLength; i++) {
+            if ((taskList[i].nextTimeAfter(from) > from) &&
+                    (taskList[i].nextTimeAfter(from) <= to)) {
+                incomingList[i] = taskList[i];
+            }
+        }
+        // Optimization of the size of incoming array
+        int realSize    =   size();         // real size of taskList
+        int index       =   0;              // index for tempList
+        Task[] tempList    =   new Task[realSize];
+        for (i = 0; i < listLength; i++) {
+            if (taskList[i] != null) {
+                tempList[index] =   taskList[i];
+                index++;
+            }
+        }
+        incomingList = tempList;
+        return incomingList;
+    }
+}
---
+++ Task.java
@@ -0,0 +1,162 @@
+//package ua.sumdu.j2se.plachkovskyy.tasks;
+
+/**
+ * Class Task for some real task
+ */
+public class Task {
+
+    private String  title;      // name of task
+    private int     time;       // time of task without of repeat
+    private int     start;      // start-time of task with repeat
+    private int     end;        // end-time of task with repeat
+    private int     interval;   // interval-time of task with repeat
+    private boolean active;     // the task is active right now
+    private boolean repeated;   // the task is repeated
+
+    /**
+     * Constructor1: inactive Task with parameters
+     */
+    public  Task(String title, int time) {
+        this.title      =   title;
+        this.time       =   time;
+//        this.active     =   false;  // not necessary initialization
+//        this.repeated   =   false;  // not necessary initialization
+    }
+
+    /**
+     * Constructor2 of the Task with parameters
+     */
+    public  Task(String title, int start, int end, int interval) {
+        this.title      =   title;
+        this.start      =   start;
+        this.end        =   end;
+        this.interval   =   interval;
+//        this.active     =   false;  // not necessary initialization
+        this.repeated   =   true;
+    }
+
+    /**
+     * get title method
+     */
+    public String getTitle() {
+        return title;
+    }
+
+    /**
+     * set title method
+     */
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    /**
+     * get active method
+     */
+    public boolean isActive() {
+        return active;
+
+    }
+
+    /**
+     * set active method
+     */
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    /**
+     * if repeated, return start
+     */
+    public int getTime() {
+        if (!repeated) {
+            return time;
+        } else {
+            return start;
+        }
+    }
+
+    /**
+     * if repeated, make it nonrepeated
+     */
+    public void setTime(int time) {
+        this.time       =   time;
+        repeated        =   false;
+    }
+
+    /**
+     * if nonrepeated, return time
+     */
+    public int getStartTime() {
+        if (repeated) {
+            return start;
+        } else {
+            return time;
+        }
+    }
+
+    /**
+     * if nonrepeated, return time
+     */
+    public int getEndTime() {
+        if (repeated) {
+            return end;
+        } else {
+            return time;
+        }
+    }
+
+    /**
+     * if nonrepeated, return 0
+     */
+    public int getRepeatInterval() {
+        if (repeated) {
+            return interval;
+        } else {
+            return 0;
+        }
+    }
+
+    /**
+     * if nonrepeated, make it repeated
+     */
+    public void setTime(int start, int end, int interval) {
+        if (!repeated) {
+            repeated = true;
+        }
+        this.start      =   start;
+        this.end        =   end;
+        this.interval   =   interval;
+    }
+
+    /**
+     * if it is repeated
+     */
+    public boolean isRepeated() {
+        return repeated;
+    }
+
+    /**
+     * if possible, return next time or start, or return -1, if impossible
+     */
+    public int nextTimeAfter(int current) {
+        if (!active) {
+            return -1;
+        } else if (!repeated) {
+            if (current >= time) {
+                return -1;
+            } else {
+                return time;
+            }
+        } else {
+            if (current < start) {
+                return start;
+            } else if (current >= end) {
+                return -1;
+            } else if ((((end - start) / interval) * interval) <= current) {
+                return -1;
+            } else {
+                return start + (((current - start) / interval) + 1) * interval;
+            }
+        }
+    }
+}
